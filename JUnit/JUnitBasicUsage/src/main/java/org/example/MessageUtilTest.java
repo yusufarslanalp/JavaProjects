48fcbb8c3dc7270efbca3c31f4d72b4e8b273fd7
yusuf_arslanalp <yusufabdullah.arslanalp@telenity.com>
2023-01-06 11:12:20 +0300

    feat: test a class from main

---
+++ JUnit/JUnitBasicUsage/src/main/java/org/example/MessageUtilTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MessageUtilTest {
+
+    String message = "Hello World";
+    MessageUtil messageUtil = new MessageUtil(message);
+
+    @Test
+    public void testPrintMessage() {
+        assertEquals(message,messageUtil.printMessage());
+    }
+}
